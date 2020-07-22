b8c54104c50af7089b7f7318e6150e660a471f45
clark2668 <baclark@msu.edu>
2020-07-22 14:24:17 -0400

    store a working copy in template.py should anything go wrong later

---
+++ python/template.py
@@ -0,0 +1,15 @@
+from icecube import icetray
+
+class SignalProp(icetray.I3Module):
+	def __init__(self, context):
+		icetray.I3Module.__init__(self, context)
+		self.default_param_value = 42
+		self.AddParameter("SomeParam", "Docstrin...", self.default_param_value)
+
+	def Configure(self):
+		self.some_param = self.GetParameter("SomeParam")
+		if self.some_param != self.default_param_value:
+			print("User changed SomeParam to {}".format(self.some_param))
+
+	def DAQ(self, frame):
+		self.PushFrame(frame)
