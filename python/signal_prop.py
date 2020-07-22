9ee4b66d952f38f9c45beb94037326deb0a61ed8
clark2668 <baclark@msu.edu>
2020-07-22 12:49:58 -0400

    add most minimal working example of a i3module possible, just to see if we can...

---
+++ python/signal_prop.py
@@ -0,0 +1,16 @@
+from I3Tray import I3Tray
+from icecube import icetray, dataio, dataclasses
+
+class SignalProp(icetray.I3Module):
+	def __init__(self, context):
+		icetray.I3Module.__init__(self, context):
+			self.default_param_value = 42
+			self.AddParameter("SomeParam", "Docstrin...", default_param_value)
+
+	def Configure(self):
+		self.some_param = self.GetParameter("SomeParam")
+		if self.some_param != self.default_param_value:
+			print("User changed SomeParam to {}".format(self.some_param))
+
+	def DAQ(self, frame):
+		self.PushFrame(frame)
