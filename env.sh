d98f6c1ed6cbd6109acd745dd6ae33652b4eff8f
clark2668 <baclark@msu.edu>
2020-07-22 18:23:27 -0400

    add an environment that sets up the nuradio dependencies correctly

---
+++ env.sh
@@ -0,0 +1,4 @@
+export local_path="/mnt/home/baclark/IceCube/new_ehe/new/metaproject/src/icetradio"
+export PYTHONPATH=$local_path/deps/NuRadioMC:$PYTHONPATH
+export PYTHONPATH=$local_path/deps/NuRadioReco:$PYTHONPATH
+export PYTHONPATH=$local_path/deps/radiotools:$PYTHONPATH
