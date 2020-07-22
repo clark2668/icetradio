28e13c5e8a77c7814c70a2d15138934d423c5e5d
clark2668 <baclark@msu.edu>
2020-07-22 18:50:27 -0400

    move submodules from deps to extern in the env.sh file

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export local_path="/mnt/home/baclark/IceCube/new_ehe/new/metaproject/src/icetradio"
-export PYTHONPATH=$local_path/deps/NuRadioMC:$PYTHONPATH
-export PYTHONPATH=$local_path/deps/NuRadioReco:$PYTHONPATH
-export PYTHONPATH=$local_path/deps/radiotools:$PYTHONPATH
+export PYTHONPATH=$local_path/extern/NuRadioMC:$PYTHONPATH
+export PYTHONPATH=$local_path/extern/NuRadioReco:$PYTHONPATH
+export PYTHONPATH=$local_path/extern/radiotools:$PYTHONPATH
