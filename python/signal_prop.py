5c55a41a7341d2fcd1517fab388d27a05708a21f
clark2668 <baclark@msu.edu>
2020-07-22 18:23:46 -0400

    start a working version that load the nuradiomc propagator

--- python/signal_prop.py
+++ python/signal_prop.py
@@ -1,15 +1,22 @@
 from icecube import icetray
 
+# we need the NuRadioMC signal propagator module
+from NuRadioMC.SignalProp import propagation
+
 class SignalProp(icetray.I3Module):
 	def __init__(self, context):
 		icetray.I3Module.__init__(self, context)
-		self.default_param_value = 42
-		self.AddParameter("SomeParam", "Docstrin...", self.default_param_value)
+		self._default_prop_mode='analytic'
+		self.AddParameter("propagation_model", 
+			"What type of propagation. Only analytic supported currently",
+			self._default_prop_mode)
 
 	def Configure(self):
-		self.some_param = self.GetParameter("SomeParam")
-		if self.some_param != self.default_param_value:
-			print("User changed SomeParam to {}".format(self.some_param))
+		self.prop_model = self.GetParameter("propagation_model")
+		if self.prop_model != self._default_prop_mode:
+			print("propagation_model changed to {}".format(self.prop_model))
+
+		self.propagator = propagation.get_propagation_module(self.prop_model)
 
 	def DAQ(self, frame):
 		self.PushFrame(frame)
