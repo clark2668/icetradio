6a68e226f3e8255f9f1194cf7e70d8db0ccbf009
clark2668 <baclark@msu.edu>
2020-07-22 13:51:50 -0400

    fix misunderstanding about how to invoke icetray in init

--- python/signal_prop.py
+++ python/signal_prop.py
@@ -1,11 +1,10 @@
-from I3Tray import I3Tray
 from icecube import icetray, dataio, dataclasses
 
 class SignalProp(icetray.I3Module):
 	def __init__(self, context):
-		icetray.I3Module.__init__(self, context):
-			self.default_param_value = 42
-			self.AddParameter("SomeParam", "Docstrin...", default_param_value)
+		icetray.I3Module.__init__(self, context)
+		self.default_param_value = 42
+		self.AddParameter("SomeParam", "Docstrin...", default_param_value)
 
 	def Configure(self):
 		self.some_param = self.GetParameter("SomeParam")
