0bfe92f8366c49f0e7c2e8dac90e30a1564fe2c0
clark2668 <baclark@msu.edu>
2020-07-24 18:24:47 -0400

    make the test script have a source with p frame so we can do stuff in physics methods

--- resources/scripts/test.py
+++ resources/scripts/test.py
@@ -1,8 +1,12 @@
-from icecube import icetray, dataio, dataclasses, icetradio
+from icecube import icetray, dataio, dataclasses, phys_services, icetradio
 from I3Tray import I3Tray
 from icecube.icetradio import signal_prop
 
+
 tray = I3Tray()
+# tray.context['I3RandomService'] = phys_services.I3GSLRandomService(42)
+tray.Add("I3InfiniteSource", stream=icetray.I3Frame.Physics) #add an infinite source in a P-frame to play with
 tray.AddModule(signal_prop.SignalProp, "SignalPropMod")
 tray.Add("Dump")
+tray.Add("I3Writer", filename="quick.i3.zst")
 tray.Execute(10)
