8336035079711253c43f94b593d77c6cf2622667
clark2668 <baclark@msu.edu>
2020-07-28 19:07:32 -0400

    update test to include signal gen module

--- resources/scripts/test.py
+++ resources/scripts/test.py
@@ -1,12 +1,14 @@
 from icecube import icetray, dataio, dataclasses, phys_services
 from I3Tray import I3Tray
-from icecube.icetradio import signal_prop
+from icecube.icetradio import signal_prop, signal_gen
 
 
 tray = I3Tray()
+tray.AddModule("I3Reader", filename='numu_sample.i3.zst')
 # tray.context['I3RandomService'] = phys_services.I3GSLRandomService(42)
-tray.Add("I3InfiniteSource", stream=icetray.I3Frame.Physics) #add an infinite source in a P-frame to play with
-tray.AddModule(signal_prop.SignalProp, "SignalPropMod")
+# tray.Add("I3InfiniteSource", stream=icetray.I3Frame.Physics) #add an infinite source in a P-frame to play with
+# tray.AddModule(signal_prop.SignalProp, "SignalPropMod")
+tray.AddModule(signal_gen.SignalGen, "SignalGen")
 tray.Add("Dump")
 tray.Add("I3Writer", filename="quick.i3.zst")
-tray.Execute(10)
+tray.Execute(6)
