1fdc02dbb1331b77f5a754aa7d45e1ba5a012ff5
clark2668 <baclark@msu.edu>
2020-07-28 19:07:13 -0400

    acctually start the signal gen module

--- python/signal_gen.py
+++ python/signal_gen.py
@@ -1,7 +1,9 @@
 # python includes
 import numpy as np
 
-from icecube import icetray
+from icecube import icetray, dataclasses
+from icecube.icetray import I3Units
+
 
 # we need the NuRadioMC signal propagator module
 from NuRadioMC.SignalProp import propagation
@@ -12,73 +14,39 @@ class SignalGen(icetray.I3Module):
 	def __init__(self, context):
 		icetray.I3Module.__init__(self, context)
 		
-		self._default_prop_mode='analytic'
-		self._default_ice_model='ARAsim_southpole'
-		self._default_att_model='SP1'
+		# self._default_prop_mode='analytic'
 		
-		self.AddParameter("propagation_model", 
-			"What type of propagation. Only analytic supported currently",
-			self._default_prop_mode)
-		
-		self.AddParameter("ice_model", 
-			"Index of refraction profile",
-			self._default_ice_model)
-		
-		self.AddParameter("attenuation_model", 
-			"Attenuation model",
-			self._default_att_model)
-
-	# do trace will actually do the ray tracing heavy lifting as an interface
-	def do_trace(self):
-		# x1 is "source" position, while x2 is the target
-		x1 = np.array([478, 0, -149])
-		x2 = np.array([635, 0, -5])
-	
-		# call the propagator for this source  & target combination
-		r = self.propagator(x1,
-							x2,
-							medium=self.ice,
-							attenuation_model=self._att_model,
-							n_frequencies_integration=25, # set this to 25
-							n_reflections=0
-							)
-
-		# now, we check for solutions
-		r.find_solutions()
-		num_solutions = r.get_number_of_solutions()
-		print("Number of solutions {}".format(num_solutions))
-
-
-		#TODO: put in logic for what happens if there is no solution
-
-
-		# we only need a few things from the ray tracer at this phase
-		for iS in range(r.get_number_of_solutions()):
-			C0 = r.get_results()[iS]['C0']
-			C1 = r.get_results()[iS]['C1']
-			sol_type = r.get_solution_type(iS)
-			path_length = r.get_path_length(iS)
-			travel_time = r.get_travel_time(iS)
-			launch_vector = r.get_launch_vector(iS)
-			receive_vector = r.get_receive_vector(iS)
-
-
+		# self.AddParameter("propagation_model", 
+		# 	"What type of propagation. Only analytic supported currently",
+		# 	self._default_prop_mode)
 
 	def Configure(self):
-		
-		self._prop_model = self.GetParameter("propagation_model")
-		self._ice_model = self.GetParameter("ice_model")
-		self._att_model = self.GetParameter("attenuation_model")
+		self.something=2
 
-		# get the propagator from NuRadioMC
-		self.propagator = propagation.get_propagation_module(self._prop_model)
-		
-		# get the ice from NuRadioMC
-		self.ice = medium.get_ice_model(self._ice_model)
+	def recover_factors(self, frame):
 
-		self.do_trace()
+		mctree = frame.Get('I3MCTree') # get the I3MCTree
+		print(mctree)
+
+		""" First, recover information about the primary
+		"""
+
+		# primary = dataclasses.get_most_energetic_primary(mctree)
+		# pnu = primary.energy/I3Units.eV
+		# pnu = primary.energy
+		for particle in mctree:
+			print(particle)
+
+		# print(primary)
+
+		# print(primary)
+		# print(primary.energy)
+		# primary = mctree.get_most_energetic_primary()
+		# print(primary.Get('energy'))
+		# for particle in frame.Get('I3MCTree'):
 
 	def DAQ(self, frame):
+		self.recover_factors(frame)
 		self.PushFrame(frame)
 
 
