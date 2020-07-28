dcf55628de1e5b88e1b16b78bc2ec1862d0ad82e
clark2668 <baclark@msu.edu>
2020-07-28 19:06:19 -0400

    add Emacs alias

--- env.sh
+++ env.sh
@@ -2,3 +2,5 @@ export local_path="/mnt/home/baclark/IceCube/new_ehe/new/metaproject/src/icetrad
 export PYTHONPATH=$local_path/extern/NuRadioMC:$PYTHONPATH
 export PYTHONPATH=$local_path/extern/NuRadioReco:$PYTHONPATH
 export PYTHONPATH=$local_path/extern/radiotools:$PYTHONPATH
+
+alias Emacs=emacs-24.3-nox
