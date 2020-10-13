d51f3f4bd5c8f1e01b55f2ab0a996f40d1fa26f2
clark2668 <baclark@msu.edu>
2020-10-13 12:33:48 -0400

    get first fully working version of signal generation, including writing traces to file

--- python/signal_gen.py
+++ python/signal_gen.py
@@ -2,11 +2,16 @@
 import numpy as np
 
 # icecube includes
-from icecube import icetray, dataclasses
+from icecube import icetray, dataclasses, icetradio
 from icecube.dataclasses import I3Particle
+from icecube.icetradio import util_geo
 
 # NuRadioMC includes
 from NuRadioMC.SignalGen import askaryan
+from radiotools import helper as hp
+from radiotools import coordinatesystems as cstrans
+from NuRadioMC.SignalProp import propagation
+from NuRadioMC.utilities import medium
 
 class SignalGen(icetray.I3Module):
 	
@@ -77,29 +82,116 @@ class SignalGen(icetray.I3Module):
 		self._dt = 1./ self._sampling_rate
 		icetray.logging.log_debug("Askaryan dt is {}".format(self._dt))
 		
-	def get_emission(self, frame):
+	def get_test_emission(self, frame):
+
+		propagator = propagation.get_propagation_module('analytic')
+		ice = medium.get_ice_model('ARAsim_southpole')
+
+		# work up a simple example to test functionality
+		ant = np.array([0,0,-100])
+		vertex = np.array([-2543.18,2319.96,-1828.74])
+		azi = 2.4658
+		zen = 1.0863
+		inelast = 0.55
+		inttype = 'cc'
+		flavor = 14
+		energy = 9.20e+18
+		n_index = ice.get_index_of_refraction(vertex)
+		cherenkov_angle = np.arccos(1./n_index)
+		shower_axis = -1 * hp.spherical_to_cartesian(zen, azi)
+
+		r = propagator(vertex,
+			ant,
+			medium=ice,
+			attenuation_model='SP1',
+			n_frequencies_integration=25,
+			n_reflections=0
+			)
+		r.find_solutions()
+		num_solutions = r.get_number_of_solutions()
+		viewing_angles = []
+		distances = []
+		launch_vectors = []
+		receive_vectors = []
+		# print('num solutions is {}'.format(num_solutions))
+		for iS in range(num_solutions):
+			launch_vectors.append(r.get_launch_vector(iS))
+			receive_vectors.append(r.get_receive_vector(iS))
+			# launch_vector = r.get_launch_vector(iS)
+			viewing_angles.append(hp.get_angle(shower_axis, launch_vectors[iS]))
+			distances.append(r.get_path_length(iS))
+			# viewing_angle = hp.get_angle(shower_axis, launch_vector)
+
+		#fem, fhad = helper._get_em_had_fraction(inelast, inttype, flavor)
+		fem=0
+		fhad=0.55
+
+		signal = askaryan.get_time_trace(
+			energy = energy * fhad,
+			theta = viewing_angles[0],
+			N = self._n_samples,
+			dt = self._dt,
+			shower_type='HAD',
+			n_index = n_index,
+			R=distances[0],
+			model=self._askaryan_model,
+			seed=self._seed
+		)
+
+		polarization_direction_onsky = util_geo.calculate_polarization_vector(launch_vectors[0], shower_axis)
+		icetray.logging.log_debug("Polarization direction on sky {}".format(polarization_direction_onsky))
+
+		this_eR, this_eTheta, this_ePhi = np.outer(polarization_direction_onsky, signal)
+
+		# create traces for the eR, eTheta, and ePhi components inside
+		eR = icetradio.I3Trace()
+		eTheta = icetradio.I3Trace()
+		ePhi = icetradio.I3Trace()
+
+		eR.trace = this_eR
+		eR.traceStartTime = 0
+		eR.samplingRate = self._sampling_rate
+		eTheta.trace = this_eTheta
+		eTheta.traceStartTime = 0
+		eTheta.samplingRate = self._sampling_rate
+		ePhi.trace = this_ePhi
+		ePhi.traceStartTime = 0
+		eTheta.samplingRate = self._sampling_rate
+
+
+		# put those traces inside an EField
+		field = icetradio.I3EField()
+		field.eR = eR
+		field.eTheta = eTheta
+		field.ePhi = ePhi
+
+		frame.Put("DummyEField", field)
+
+
 
 		# for more information on how the askaryan module works
 		# and it's various parameters, please consult NuRadioMC
 		# https://github.com/nu-radio/NuRadioMC/blob/master/NuRadioMC/SignalGen/askaryan.py
 
-		nindex = 1.35
-		thetac = np.arccos(1/nindex)
+		# nindex = 1.35
+		# thetac = np.arccos(1/nindex)
 
-		signal = askaryan.get_time_trace(
-			energy = 1e18,
-			theta = thetac,
-			N=self._n_samples,
-			dt=self._dt,
-			shower_type='HAD',
-			n_index=nindex,
-			R=1,
-			model=self._askaryan_model,
-			seed=self._seed
-		)
+		# signal = askaryan.get_time_trace(
+		# 	energy = 1e18,
+		# 	theta = thetac,
+		# 	N=self._n_samples,
+		# 	dt=self._dt,
+		# 	shower_type='HAD',
+		# 	n_index=nindex,
+		# 	R=1,
+		# 	model=self._askaryan_model,
+		# 	seed=self._seed
+		# )
+
+
 
 	def Physics(self, frame):
-		# self.get_emission(frame)
+		self.get_test_emission(frame)
 		self.PushFrame(frame)
 
 
