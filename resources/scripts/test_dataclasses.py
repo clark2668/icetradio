3a97e157b58343c952bf3733ab2c3d434e76587a
clark2668 <baclark@msu.edu>
2020-10-13 12:35:17 -0400

    add small test script to exercise dataclasses

---
+++ resources/scripts/test_dataclasses.py
@@ -0,0 +1,9 @@
+import math
+
+from icecube import icetray, dataio, dataclasses, phys_services, icetradio
+from I3Tray import I3Tray
+
+# a = dataclasses.I3Particle()
+
+# a_hammer = icetradio.I3Hammer()
+# a_hammer.SetHeight(2)
