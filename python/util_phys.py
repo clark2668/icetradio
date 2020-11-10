95dc1e947508742b32aaa0d4e537f7ddb340d007
clark2668 <baclark@msu.edu>
2020-11-10 18:01:25 -0500

    update the physics utility too

---
+++ python/util_phys.py
@@ -0,0 +1,15 @@
+import numpy as np
+from icecube import icetray, dataclasses
+from icecube.dataclasses import I3Particle
+
+def pick_em_or_had(type):
+
+	em_types = [I3Particle.EMinus, I3Particle.NuclInt, I3Particle.Brems]
+	had_types = [I3Particle.Hadrons]
+
+	if type in had_types:
+		return 'had'
+	elif type in em_types:
+		return 'em'
+	else:
+		return 'udef'
