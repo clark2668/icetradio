2b2e5afb6223a234372972959ab8bfaddc311295
clark2668 <baclark@msu.edu>
2020-11-11 14:55:19 -0500

    nuclint was misclassified in the physics utility

--- python/util_phys.py
+++ python/util_phys.py
@@ -4,8 +4,8 @@ from icecube.dataclasses import I3Particle
 
 def pick_em_or_had(type):
 
-	em_types = [I3Particle.EMinus, I3Particle.NuclInt, I3Particle.Brems]
-	had_types = [I3Particle.Hadrons]
+	em_types = [I3Particle.EMinus, I3Particle.Brems]
+	had_types = [I3Particle.Hadrons, I3Particle.NuclInt]
 
 	if type in had_types:
 		return 'had'
