d244e3cd35edecb1d3deefdb9b0a695045f6d5e9
clark2668 <baclark@msu.edu>
2020-12-16 14:21:48 -0500

    icetradio needs its own environment variable

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
-export local_path="/mnt/home/baclark/IceCube/new_ehe/new/metaproject/src/icetradio"
+export local_path="/mnt/home/baclark/IceCube/new_ehe/new/radio_metaproject/src/icetradio"
 export PYTHONPATH=$local_path/extern/NuRadioMC:$PYTHONPATH
 export PYTHONPATH=$local_path/extern/NuRadioReco:$PYTHONPATH
 export PYTHONPATH=$local_path/extern/radiotools:$PYTHONPATH
+export icetradio_path=$local_path
 
 #for requests
 export PYTHONPATH=/mnt/home/baclark/IceCube/new_ehe/new/deps/pip_installed_things:$PYTHONPATH
